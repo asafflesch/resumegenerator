3b6b17a634eea4180bec19578ea85c8fe58c4eaf
Faqa <Faqa@localhost>
2013-09-20 22:27:50 +0300

    Created final version + deleted a few files

--- CompileResume.py
+++ CompileResume.py
@@ -1,6 +1,8 @@
 from Cheetah.Template import Template
+from optparse import OptionParser
 import xml.etree.ElementTree as ET
 import MyFilters
+import os
 
 class ContactInfo(object):
 	def __init__(self):
@@ -20,20 +22,34 @@ class CatItem(object):
 		self.text = ""
 
 if __name__ == '__main__':
+	parser = OptionParser(usage = "%prog [private] <fileTypes> <resumeFile>", 
+		description = "Takes the colon-separated list <fileTypes> and for each type, fills in the Cheetah template ResTemp<Type>.tmpl according to the data in ResumeFile")
+	parser.add_option("-p", "--addprivateacontactdata", action="store_true", dest="addprivate", default = False)
+	options, manArgs = parser.parse_args()
+
+	if (len(manArgs) < 2):
+		parser.print_help()
+		exit()
+
+
+	allOutputTypes = manArgs[0]
+	outputList = allOutputTypes.split(":")
+
+	inputFile = manArgs[1]
+
 	searchList = {}
 	
-	tree = ET.parse("Resume.xml")
+	tree = ET.parse(inputFile)
 	
 	root = tree.getroot()
 	
 	searchList['name'] = root.find("Name").text
+	cInfo = root.find("ContactInfo")
+	outputStr = cInfo.find("PublicInfo").text
+	if (options.addprivate):
+		outputStr = outputStr + "{cr}" + cInfo.find("PrivateInfo").text
 
-	info = root.find("ContactInfo")
-	cinfo = ContactInfo()
-	cinfo.misc = info.find("Misc").text
-	cinfo.email = info.find("Email").text
-	searchList['contactInfo'] = cinfo
-
+	searchList['contactInfo'] = outputStr
 	categories = []
 	for category in root.iter("Category"):
 		newCat = Category()
@@ -59,6 +75,18 @@ if __name__ == '__main__':
 
 	searchList["categories"] = categories
 
-	htmltemplate = Template(file="ResTempHTML.tmpl", searchList = searchList, filtersLib = MyFilters)
-	latextemplate = Template(file="ResTempTex.tmpl", searchList = searchList, filtersLib = MyFilters)
-	
+	for outputType in outputList:
+		templatePath = "restemp%s.tmpl" % outputType
+		if (os.path.exists (templatePath)):
+			print "Writing using template %s" % templatePath
+			template = Template(file = templatePath, searchList = searchList, filtersLib = MyFilters)
+			currFile = open("Resume.%s" % outputType, 'w')
+			currFile.write(str(template))
+			currFile.close()
+			print "Written to Resume.%s" % outputType
+		else:
+			print "%s could not be found" % templatePath
+
+
+
+
